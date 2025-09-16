ed2ded7e93d5b7214905eb79e5c9ee72f271fe85
d <d'@DESKTOP-6BHOUR4>
2025-09-16 16:21:40 -0300

    Method GET

---
+++ WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<ProductModel>> SearchProduct()
+        {
+            var products = _context.Products.ToList();
+            return Ok(products);
+        }
+    }
+}
